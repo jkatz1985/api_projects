58167fdce54f994b4d6e527d795600a7d3e05d91
Jacob Katz <jacob-katz@kellogg.northwestern.edu>
2015-04-14 18:21:08 -0500

    api.4

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -4,8 +4,8 @@ require 'json'
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 puts "Let's get the weather forecast for your address."
 
@@ -18,6 +18,10 @@ url_safe_street_address = URI.encode(street_address)
 url_weather_data = "https://api.forecast.io/forecast/5c48999e1c9b93f9fd0134d755bf0271"+url_safe_street_address
 raw_weather_data = open(url_weather_data).read
 parsed_weather_data = JSON.parse(raw_weather_data)
+
+the_hour_outlook = parsed_weather_data["hourly"]["data"][1]["summary"]
+the_day_outlook = parsed_weather_data["daily"]["data"][1]["summary"]
+the_temperature = parsed_weather_data["currently"]["temperature"]
 
 # Ultimately, we want the following line to work when uncommented:
 
